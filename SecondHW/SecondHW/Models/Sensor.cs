43404e7821b75241396b8f8d6b8438f5093ee824
sealPIG <69681501+sealPIG@users.noreply.github.com>
2022-09-07 13:37:01 +0800

    Add files via upload

---
+++ SecondHW/SecondHW/Models/Sensor.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_V2.Models
+{
+    public class Sensor
+    {
+        public int SensorId { get; set; }
+
+        public string? SensorName { get; set; }
+
+        public int SensorValue { get; set; }
+    }
+}
